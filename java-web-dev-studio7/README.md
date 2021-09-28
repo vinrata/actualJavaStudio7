8c922cc688d76a105772000ed095d0d4e93b707f
vinrata <57158224+vinrata@users.noreply.github.com>
2021-09-27 23:11:18 -0500

    Add files via upload

---
+++ java-web-dev-studio7/README.md
@@ -0,0 +1,1 @@
+# actualJavaStudio7
---
+++ java-web-dev-studio7/src/org/launchcode/studio7/Main.java
@@ -0,0 +1,12 @@
+package org.launchcode.studio7;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        // TODO: Declare and initialize a CD and a DVD object.
+
+        // TODO: Call each CD and DVD method to verify that they work as expected.
+
+    }
+}
